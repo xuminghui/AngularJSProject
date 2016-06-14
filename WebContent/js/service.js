d75fffc696014262c11da8ba2cebc82839bb7a5c
xuminghui <xuminghui18610740807@gmail.com>
2016-06-14 18:00:07 +0800

    test

--- WebContent/js/service.js
+++ WebContent/js/service.js
@@ -1,11 +1,30 @@
-angular.module('myApp', []).factory('greeter', function() {
+
+angular.module('myApp', []).value('url','js/data.txt').factory('helloService', ['$http','url',function($http,url) {
 	return {
-		greet:function(msg){
-			alert(msg);
+		getPersons:function(){
+			return $http.get(url);
 		}
-	}
-}).controller('serviceController',['$scope','greeter',function($scope,greeter){
-	$scope.sayHello=function(){
-		greeter.greet('Hello');
-	}
-}]);
+	};
+}]).controller(
+		'serviceController',
+		[ '$scope', '$timeout', 'helloService',
+				function($scope, $timeout, helloService) {
+					var timeout;
+					$scope.$watch('username', function(newUsername) {
+						//timeout表示一个promise，如果当前进程存在一个timeout，则取消，并按照下面代码再执行生成一个Promise;
+						//然后延迟指定时间，处理function
+						if (timeout){
+							$timeout.cancel(timeout);
+						}
+							
+						timeout = $timeout(function() {
+							helloService.getPersons().then(function(response) {
+								alert('获取查询结果');
+								$scope.persons = response.data;
+							}, function(response) {
+								alert('error');
+							});
+						}, 2000);
+
+					});
+				} ]);
